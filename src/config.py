9569fd10417c27923add74c4dff24f2780a638f4
hooji nguyen <vanhoinguyen98@gmail.com>
2026-07-02 23:19:28 +0700

    fix: align fallback database user and name with upstream docker-compose defaults

--- src/config.py
+++ src/config.py
@@ -7,8 +7,8 @@ ZAMMAD_COMPOSE_REPO = "https://github.com/zammad/zammad-docker-compose.git"
 DEFAULT_WEB_PORT = 8080
 
 # Default PostgreSQL database settings
-DB_DEFAULT_USER = "zammad_rw"
-DB_DEFAULT_NAME = "zammad"
+DB_DEFAULT_USER = "zammad"
+DB_DEFAULT_NAME = "zammad_production"
 
 # Elasticsearch System requirements for Linux kernel parameters
 REQUIRED_VM_MAX_MAP_COUNT = 262144
